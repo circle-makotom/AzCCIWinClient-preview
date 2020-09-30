48d32ed94d4f180a7869f30eed5f40a0f1676ed3
Makoto Mizukami <makoto@circleci.com>
2020-09-30 15:33:07 +0900

    Say hi!

--- AzCCIWinClientTest/SayHelloTest.cs
+++ AzCCIWinClientTest/SayHelloTest.cs
@@ -12,13 +12,13 @@
             string[] testedName = { "Alice", "Bob", string.Empty };
             var inst = new SayHello(testedName[0]);
 
-            Assert.AreEqual($"Bonjour, {testedName[0]}! Welcome to this app.", inst.GreetingMessage());
+            Assert.AreEqual($"Hi, {testedName[0]}! Welcome to this app.", inst.GreetingMessage());
 
             inst.User = testedName[1];
-            Assert.AreEqual($"Bonjour, {testedName[1]}! Welcome to this app.", inst.GreetingMessage());
+            Assert.AreEqual($"Hi, {testedName[1]}! Welcome to this app.", inst.GreetingMessage());
 
             inst.User = testedName[2];
-            Assert.AreEqual($"Bonjour, anonymous! Welcome to this app.", inst.GreetingMessage());
+            Assert.AreEqual($"Hi, anonymous! Welcome to this app.", inst.GreetingMessage());
         }
     }
 }
